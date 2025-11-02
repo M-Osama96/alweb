f84cb81eff19acffeff82488abdf2080e915e152
M-Osama96 <mohamedosama3466@gmail.com>
2025-11-03 00:01:37 +0200

    services-circled

---
+++ services-circled/js/main.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    575: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
