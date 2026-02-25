3288841b28feea96eaff6618bbaf8235622ca8cd
M-Osama96 <mohamedosama3466@gmail.com>
2026-02-26 00:08:49 +0200

    banner33

--- banner3/js/main.js
+++ banner3/js/main.js
@@ -1,26 +1,3 @@
 Fancybox.bind("[data-fancybox]", {
   //
 });
-
-var projects1swiper = new Swiper(".projects1Swiper", {
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".projects-circl1 .swiper-button-next",
-    prevEl: ".projects-circl1 .swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-  },
-});
