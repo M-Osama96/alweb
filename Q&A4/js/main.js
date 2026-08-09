2056918d099cd173cad4b05364f46c351fdec245
M-Osama96 <mohamedosama3466@gmail.com>
2026-08-10 00:58:46 +0300

    footer form

---
+++ Q&A4/js/main.js
@@ -0,0 +1,21 @@
+Fancybox.bind("[data-fancybox]", {
+  //
+});
+if (document.querySelectorAll(".accordion-header") !== null) {
+  const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+  accordionHeaders.forEach((header) => {
+    header.addEventListener("click", () => {
+      const item = header.parentElement;
+      const openItem = document.querySelector(".accordion-item.active-a");
+
+      // لو فيه عنصر مفتوح غير الحالي → يقفله
+      if (openItem && openItem !== item) {
+        openItem.classList.remove("active-a");
+      }
+
+      // يقلب حالة العنصر الحالي (فتح / غلق)
+      item.classList.toggle("active-a");
+    });
+  });
+}
